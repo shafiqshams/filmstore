e4222f9c61ff62c8eba68f06f82a0438ae434e5b
Shafiq Shams <sshams@north.io>
2022-08-15 20:20:48 +0200

    SafeAreaView and StatusBar added in App root

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import {StatusBar, SafeAreaView} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {AppNavigator} from './src/routes/MainNavigator';
+import colors from './src/styles/colors';
 
 const App = () => {
   return (
     <NavigationContainer>
+      <SafeAreaView style={{flex: 0, backgroundColor: colors.black01}} />
+
+      <StatusBar
+        barStyle="dark-content"
+        hidden={false}
+        backgroundColor={colors.black01}
+        translucent={true}
+      />
+
       <AppNavigator />
     </NavigationContainer>
   );
