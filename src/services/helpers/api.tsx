6be79a4d6bcd9a3042eb86d12eb0cfe2f0ef9461
Shafiq Shams <sshams@north.io>
2022-08-16 18:16:03 +0200

    Fetch implementation

---
+++ src/services/helpers/api.tsx
@@ -0,0 +1,19 @@
+export const getFetch = (url: string, params = {}) => {
+  const queryString = Object.entries(params)
+    .map(param => {
+      return `${param[0]}=${param[1]}`;
+    })
+    .join('&');
+
+  console.log('URL: ', `${url}?${queryString}`);
+
+  return fetch(`${url}?${queryString}`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(res => res.json())
+    .catch(err => console.error(err));
+};
