630b5e0f09c779efcc00b91544ae9116a87803f1
Shafiq Shams <sshams@north.io>
2022-08-19 00:32:20 +0200

    App name included in config file

--- src/services/helpers/config.tsx
+++ src/services/helpers/config.tsx
@@ -1,3 +1,4 @@
 export const BASE_URL = 'https://api.themoviedb.org/3/';
 export const API_KEY = 'cc67c8aa4c5c7f51a41d5737b4cad429';
 export const IMAGE_URL = 'https://image.tmdb.org/t/p/';
+export const APP_NAME = 'FilmStore';
