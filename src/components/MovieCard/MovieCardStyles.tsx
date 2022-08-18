a84b395dee0f2941ce7bae1cad5cde3ca72e82ac
Shafiq Shams <sshams@north.io>
2022-08-18 12:28:16 +0200

    Style fix for movie card

--- src/components/MovieCard/MovieCardStyles.tsx
+++ src/components/MovieCard/MovieCardStyles.tsx
@@ -5,12 +5,12 @@ export default StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    marginHorizontal: 7.5,
   },
   imageStyle: {
     width: 200,
     height: 300,
     resizeMode: 'contain',
     borderRadius: 24,
-    marginHorizontal: 8,
   },
 });
