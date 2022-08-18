8ff1ffe33c41cd3615eee9aca096f0e5bde3748a
Shafiq Shams <sshams@north.io>
2022-08-18 11:11:16 +0200

    Fetching category list, api call added

--- src/services/movies/movies.tsx
+++ src/services/movies/movies.tsx
@@ -1,10 +1,18 @@
 import {BASE_URL, API_KEY} from '../helpers/config';
 import {getFetch} from '../helpers/api';
 
-export function getMoviesByCategoryId(genreId: number) {
+export function getCategories() {
+  const URL = BASE_URL + 'genre/movie/list';
+  return getFetch(URL, {
+    api_key: API_KEY,
+  });
+}
+
+export function getMoviesByCategoryId(genreId: number, page: number) {
   const URL = BASE_URL + 'discover/movie';
   return getFetch(URL, {
     api_key: API_KEY,
     with_genre: genreId,
+    page: page,
   });
 }
