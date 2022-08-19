c2a492a751162f747dd8108bec09975f964f25e3
Shafiq Shams <sshams@north.io>
2022-08-19 02:01:08 +0200

    Detail page in progress, details added

--- src/scenes/DetailPage/DetailPage.tsx
+++ src/scenes/DetailPage/DetailPage.tsx
@@ -1,23 +1,34 @@
-import {View} from 'react-native';
-import React, {FC, useState, useEffect, useCallback} from 'react';
+import {View, Image, Text} from 'react-native';
+import React, {FC, useState, useEffect} from 'react';
 import styles from './DetailPageStyles';
 import {getMovie} from '../../services/movies/movies';
 import TextHeader from '../../components/TextHeader/TextHeader';
 import {MovieDetails} from '../../typings';
+const CARD_SIZE = 'w400';
+import {getImagePath} from '../../utils/getImagePath';
 
 const DetailPage: FC<any> = ({route, navigation}) => {
   const {movieId} = route.params;
   console.log('Movie ID', movieId);
   const [movieDetails, setMovieDetails] = useState<MovieDetails>();
 
-  const fetchMovie = useCallback(async () => {
-    const {id, title, tagline, overview, poster_path} = await getMovie(movieId);
-    setMovieDetails({id, title, tagline, overview, poster_path});
-  }, [movieId]);
+  // const fetchMovie = useCallback(async () => {
+  //   const {id, title, tagline, overview, poster_path} = await getMovie(movieId);
+  //   setMovieDetails({id, title, tagline, overview, poster_path});
+  // }, [movieId]);
 
   useEffect(() => {
+    console.log('UseEffect called ');
+
+    const fetchMovie = async () => {
+      const {id, title, tagline, overview, backdrop_path} = await getMovie(
+        movieId,
+      );
+      setMovieDetails({id, title, tagline, overview, backdrop_path});
+    };
+
     fetchMovie().catch(console.error);
-  }, [fetchMovie]);
+  }, [movieId]);
 
   console.log('what are details ', movieDetails);
 
@@ -32,6 +43,27 @@ const DetailPage: FC<any> = ({route, navigation}) => {
         headerText={movieDetails?.title}
         onPressFav={addToFavorite}
       />
+
+      <View style={styles.bodyContainer}>
+        {/* TODO in components */}
+        <View style={styles.taglineContainer}>
+          <Text style={styles.taglineStyles} numberOfLines={4}>
+            {movieDetails?.tagline}
+          </Text>
+        </View>
+
+        <Image
+          style={styles.imageStyle}
+          source={{uri: getImagePath(CARD_SIZE, movieDetails?.backdrop_path)}}
+        />
+
+        <View style={styles.overviewContainer}>
+          <View style={{marginBottom: 12}}>
+            <Text style={styles.overviewHeadline}>{'Overview'}</Text>
+          </View>
+          <Text style={styles.overviewStyles}>{movieDetails?.overview}</Text>
+        </View>
+      </View>
     </View>
   );
 };
--- src/scenes/DetailPage/DetailPageStyles.tsx
+++ src/scenes/DetailPage/DetailPageStyles.tsx
@@ -9,5 +9,42 @@ export default StyleSheet.create({
     backgroundColor: colors.black01,
     alignItems: 'center',
     marginTop: 20,
-  }
+  },
+  bodyContainer: {
+    flex: 1,
+    width: width - 60,
+  },
+  taglineContainer: {
+    flex: 0.15,
+    marginBottom: 15,
+  },
+  taglineStyles: {
+    textAlign: 'center',
+    fontSize: 18,
+    color: colors.white,
+    fontFamily: 'ComicSans-Italic',
+    letterSpacing: 0.2,
+  },
+  imageStyle: {
+    width: width - 60,
+    height: 200,
+    resizeMode: 'contain',
+    borderRadius: 18,
+  },
+  overviewContainer: {
+    flex: 0.85,
+    marginTop: 24,
+  },
+  overviewHeadline: {
+    fontSize: 28,
+    color: colors.red,
+    fontFamily: 'Weast-hood',
+  },
+  overviewStyles: {
+    textAlign: 'left',
+    fontSize: 16,
+    color: colors.white,
+    fontFamily: 'ComicSans-Regular',
+    letterSpacing: 0.8,
+  },
 });
